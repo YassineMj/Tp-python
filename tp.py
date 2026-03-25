81b19002391d01af5a80e58cb4f09dd942266a73
unknown <YassineMj>
2026-03-25 12:46:10 +0100

    COMMIT 4 — rank_matter_2 et rank_matter_3

--- tp.py
+++ tp.py
@@ -18,6 +18,12 @@ class SchoolClass:
 
     def rank_matter_1(self):
         return sorted(self.students, key=lambda s: s.m1, reverse=True)
+    
+    def rank_matter_2(self):
+        return sorted(self.students, key=lambda s: s.m2, reverse=True)
+
+    def rank_matter_3(self):
+        return sorted(self.students, key=lambda s: s.m3, reverse=True)
 
 
 if __name__ == "__main__":
@@ -26,3 +32,5 @@ if __name__ == "__main__":
     school_class.add_student(Student('A', 8, 2, 17))
     school_class.add_student(Student('V', 9, 14, 14))
     print(school_class.rank_matter_1())
+    print(school_class.rank_matter_2())
+    print(school_class.rank_matter_3())
