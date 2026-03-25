323398051b61d218b85a991c9ab0635e297798e4
unknown <YassineMj>
2026-03-25 12:43:28 +0100

    COMMIT 3 — rank_matter_1

--- tp.py
+++ tp.py
@@ -16,9 +16,13 @@ class SchoolClass:
     def add_student(self, student):
         self.students.append(student)
 
+    def rank_matter_1(self):
+        return sorted(self.students, key=lambda s: s.m1, reverse=True)
+
 
 if __name__ == "__main__":
     school_class = SchoolClass()
     school_class.add_student(Student('J', 10, 12, 13))
     school_class.add_student(Student('A', 8, 2, 17))
     school_class.add_student(Student('V', 9, 14, 14))
+    print(school_class.rank_matter_1())
